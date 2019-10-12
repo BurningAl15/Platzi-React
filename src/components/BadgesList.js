fc7ff010861eccc43f643beb566bdf7b5fd2e11e
Aldhair Vera <aldhairvera15@gmail.com>
2019-10-11 22:54:24 -0500

    Improving with custom hooks

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -33,14 +33,28 @@ class BadgesListItem extends React.Component {
   }
 }
 
+function useSearchBadges(badges) {
+  const [query, setQuery] = React.useState("");
+
+  const [filteredBadges, setFilterBadges] = React.useState(badges);
+
+  React.useMemo(() => {
+    const result = badges.filter(badge => {
+      return `${badge.firstName} ${badge.lastName}`
+        .toLowerCase()
+        .includes(query.toLowerCase());
+    });
+
+    setFilterBadges(result);
+  }, [badges, query]);
+
+  return { query, setQuery, filteredBadges };
+}
+
 function BadgesList(props) {
   const badges = props.badges;
 
-  const [query, setQuery] = React.useState("");
-
-  const filteredBadges = badges.filter(badge => {
-    return `${badge.firstName} ${badge.lastName}`.toLowerCase().includes(query.toLowerCase());
-  });
+  const { query, setQuery, filteredBadges } = useSearchBadges(badges);
 
   if (filteredBadges.length === 0) {
     return (
